6bad00394a0ccbb8ff0ea54ca86dc3defa2d60e7
Kseniya Pepelyaeva <C:\Users\kpepelyaeva\AppData\Roaming\The Bat!>
2025-05-27 14:24:21 +0500

    Add list form sample

---
+++ src/components/DataTable/DataTableToolbar.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Button, Divider, Icon, IconButton, Stack, Typography } from '@mui/material';
+
+interface DataTableToolbarProps {
+  handleCreateButtonClick: () => void;
+}
+
+const DataTableToolbar: React.FC<DataTableToolbarProps> = ({ handleCreateButtonClick }) => {
+  return (
+    <Stack direction="row" alignItems="center" flexWrap="wrap" mb={2.5}>
+      <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+        0 объекта(ов) выбрано
+      </Typography>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<Icon fontSize="inherit" className="icon-plus" />}
+          onClick={handleCreateButtonClick}
+        >
+          Создать
+        </Button>
+        <Button variant="contained" startIcon={<Icon className="icon-minus" />}>
+          Удалить
+        </Button>
+        <Button variant="contained" startIcon={<Icon className="icon-download" />}>
+          Экспортировать
+        </Button>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <IconButton color="primary" aria-label="delete" title="Настройки">
+          <Icon className="icon-settings" />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default DataTableToolbar;
---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { Column } from 'primereact/column';
+import { DataTable as PrimeDataTable } from 'primereact/datatable';
+import { InputText } from 'primereact/inputtext';
+import { MultiSelect } from 'primereact/multiselect';
+import DataTableToolbar from '@/components/DataTable/DataTableToolbar';
+
+import { useMemo, useState } from 'react';
+import { Typography } from '@mui/material';
+
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import './style.scss';
+
+interface DataTableProps {
+  data: any[];
+  fields: string[];
+  title: string;
+  onDelete: (index: number) => void;
+  onRowClick: (item: any) => void;
+  onCreate: () => void;
+}
+
+const DataTable = ({ data, fields, title, onDelete, onRowClick, onCreate }: DataTableProps) => {
+  const [globalFilter, setGlobalFilter] = useState('');
+  const [visibleColumns, setVisibleColumns] = useState(fields);
+  const [first, setFirst] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const columns = useMemo(
+    () =>
+      fields.map((field) => ({
+        field,
+        header: field.charAt(0).toUpperCase() + field.slice(1),
+      })),
+    [fields]
+  );
+
+  const filteredData = useMemo(() => {
+    if (!globalFilter) return data;
+    return data.filter((item) =>
+      fields.some((field) => item[field]?.toString().toLowerCase().includes(globalFilter.toLowerCase()))
+    );
+  }, [data, fields, globalFilter]);
+
+  return (
+    <>
+      <Typography variant="h2" component="h2">
+        {title}
+      </Typography>
+      <Typography variant="subtitle1">Подзаголовок</Typography>
+      <DataTableToolbar handleCreateButtonClick={onCreate} />
+      <div className="flex justify-between items-center mb-2 gap-2">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            value={globalFilter}
+            onChange={(e) => setGlobalFilter(e.target.value)}
+            placeholder="Global Search"
+          />
+        </span>
+
+        <MultiSelect
+          value={visibleColumns}
+          options={columns.map((col) => ({ label: col.header, value: col.field }))}
+          onChange={(e) => setVisibleColumns(e.value)}
+          display="chip"
+          placeholder="Show/Hide Columns"
+          className="w-full md:w-20rem"
+        />
+      </div>
+
+      <PrimeDataTable
+        value={filteredData}
+        paginator
+        first={first}
+        rows={rowsPerPage}
+        onPage={(e) => {
+          setFirst(e.first);
+          setRowsPerPage(e.rows);
+        }}
+        onRowClick={(e) => onRowClick(e.data)}
+        rowsPerPageOptions={[5, 10, 20]}
+        resizableColumns
+        columnResizeMode="fit"
+        scrollable
+        globalFilterFields={fields}
+        sortMode="multiple"
+        scrollHeight="100%"
+      >
+        {columns
+          .filter((col) => visibleColumns.includes(col.field))
+          .map((col) => (
+            <Column key={col.field} field={col.field} header={col.header} sortable />
+          ))}
+      </PrimeDataTable>
+    </>
+  );
+};
+
+export default DataTable;
